314aaed9c31009052cfa83f6607603aff2ae33d1
Sami Alzein <sami.alzein@hotmail.com>
2025-03-29 17:34:56 +0100

    adds assignment 3 part 1

--- examples/systemcalls/systemcalls.h
+++ examples/systemcalls/systemcalls.h
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdarg.h>
+#include <stdlib.h>    // For EXIT_FAILURE
+#include <unistd.h>    // For fork(), execv(), _exit()
+#include <sys/types.h> // For pid_t
+#include <sys/wait.h>  // For waitpid()
+#include <stdarg.h>    // For variable arguments (va_list)
+#include <stdbool.h>   // For bool type
+#include <fcntl.h>
 
 bool do_system(const char *command);
 
